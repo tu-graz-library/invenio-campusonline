31a67ee64f277f031e4c91be28ca043f1ee45fb2
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-12-29 14:23:18 +0100

    global: refactor
    * move the provider part to invenio-workflows-tugraz. it belongs to the
      workflow not to the connector module
    
    * add documentation
    
    * fix types

--- invenio_campusonline/ext.py
+++ invenio_campusonline/ext.py
@@ -8,10 +8,8 @@
 
 """This module is used to import/export from/to the CampusOnline System."""
 
-from . import config
 
-
-class InvenioCampusonline(object):
+class InvenioCampusonline:
     """invenio-campusonline extension."""
 
     def __init__(self, app=None):
@@ -21,25 +19,4 @@ class InvenioCampusonline(object):
 
     def init_app(self, app):
         """Flask application initialization."""
-        self.init_config(app)
         app.extensions["invenio-campusonline"] = self
-
-    def init_config(self, app):
-        """Initialize configuration."""
-        app.config.setdefault("CELERY_BEAT_SCHEDULE", {})
-        app.config.setdefault("INVENIO_MARC21_PERSISTENT_IDENTIFIER_PROVIDERS", [])
-        app.config.setdefault("INVENIO_MARC21_PERSISTENT_IDENTIFIERS", {})
-
-        for k in dir(config):
-            if k == "CAMPUSONLINE_CELERY_BEAT_SCHEDULE":
-                app.config["CELERY_BEAT_SCHEDULE"].update(getattr(config, k))
-            elif k == "CAMPUSONLINE_PERSISTENT_IDENTIFIER_PROVIDERS":
-                app.config["INVENIO_MARC21_PERSISTENT_IDENTIFIER_PROVIDERS"].extend(
-                    getattr(config, k)
-                )
-            elif k == "CAMPUSONLINE_PERSISTENT_IDENTIFIERS":
-                app.config["INVENIO_MARC21_PERSISTENT_IDENTIFIERS"].update(
-                    getattr(config, k)
-                )
-            elif k.startswith("CAMPUSONLINE_"):
-                app.config.setdefault(k, getattr(config, k))
