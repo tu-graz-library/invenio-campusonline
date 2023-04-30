cf327046195b2e5d177d7a0de67cec43518000d5
Christoph Ladurner <christoph.ladurner@tugraz.at>
2023-04-30 22:23:42 +0200

    cli: move to secho way from logger
    * secho from click seams the better solution to output the result of a
      command as the logging system. the problem was that there was no
      output on the production system. Which is ok due to the logging
      mechanism, but it was not the intention in this particular case.
      therefore now the secho command is used.

--- invenio_campusonline/cli.py
+++ invenio_campusonline/cli.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2022 Graz University of Technology.
+# Copyright (C) 2022-2023 Graz University of Technology.
 #
 # invenio-campusonline is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -8,27 +8,28 @@
 """Command line interface to interact with the CampusOnline-Connector module."""
 
 
-import click
+from click import STRING, group, option, secho
 from click_params import URL
 from flask import current_app
 from flask.cli import with_appcontext
 
 from .api import fetch_all_ids, import_from_campusonline
-from .types import CampusOnlineConfigs
+from .types import CampusOnlineConfigs, Color
 
 
-@click.group()
+@group()
 def campusonline():
     """Campusonline CLI."""
 
 
 @campusonline.command()
 @with_appcontext
-@click.option("--endpoint", type=URL)
-@click.option("--campusonline-id", type=click.STRING)
-@click.option("--token", type=click.STRING)
-@click.option("--user-email", type=click.STRING, default="cms@tugraz.at")
-def import_thesis(endpoint, campusonline_id, token, user_email):
+@option("--endpoint", type=URL)
+@option("--campusonline-id", type=STRING)
+@option("--token", type=STRING)
+@option("--user-email", type=STRING, default="cms@tugraz.at")
+@option("--no-color", is_flag=True)
+def import_thesis(endpoint, campusonline_id, token, user_email, no_color=False):
     """Import metadata and file (aka one thesis) from campusonline."""
     import_func = current_app.config["CAMPUSONLINE_IMPORT_FUNC"]
     theses_filters = current_app.config["CAMPUSONLINE_THESES_FILTERS"]
@@ -40,16 +41,19 @@ def import_thesis(endpoint, campusonline_id, token, user_email):
     )
     record = import_from_campusonline(import_func, campusonline_id, configs)
 
-    current_app.logger.info(f"record.id: {record.id}")
+    color = Color.success if not no_color else Color.neutral
+    secho(f"record.id: {record.id}", fg=color)
 
 
 @campusonline.command()
 @with_appcontext
-@click.option("--endpoint", type=URL)
-@click.option("--token", type=click.STRING)
-def fetch_ids(endpoint, token):
+@option("--endpoint", type=URL)
+@option("--token", type=STRING)
+@option("--no-color", is_flag=True)
+def fetch_ids(endpoint, token, no_color):
     """Fetch all to import ids."""
     theses_filters = current_app.config["CAMPUSONLINE_THESES_FILTERS"]
     ids = fetch_all_ids(endpoint, token, theses_filters)
 
-    current_app.logger.info(ids)
+    color = Color.success if not no_color else Color.neutral
+    secho(f"ids: {ids}", fg=color)
