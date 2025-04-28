69cd42c78803b611b5ef12f9bef3eabc83db79c7
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-04-28 21:37:00 +0200

    chore: maintenance

--- invenio_campusonline/__init__.py
+++ invenio_campusonline/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2021-2024 Graz University of Technology.
+# Copyright (C) 2021-2025 Graz University of Technology.
 #
 # invenio-campusonline is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -17,10 +17,10 @@ from .types import CampusOnlineID, ThesesFilter
 __version__ = "0.5.0"
 
 __all__ = (
-    "__version__",
-    "InvenioCampusonline",
     "CampusOnlineID",
     "CampusOnlineRESTService",
+    "InvenioCampusonline",
     "ThesesFilter",
+    "__version__",
     "current_campusonline",
 )
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2021-2023 Graz University of Technology.
+# Copyright (C) 2021-2025 Graz University of Technology.
 #
 # invenio-campusonline is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -12,30 +12,22 @@ See https://pytest-invenio.readthedocs.io/ for documentation on which test
 fixtures are available.
 """
 
-
+from collections.abc import Callable
 from pathlib import Path
 from xml.etree.ElementTree import Element, parse
 
 import pytest
-from flask import Flask
-
-from invenio_campusonline import InvenioCampusonline
-
-
-@pytest.fixture()
-def minimal_record() -> Element:
-    """Create minimal record."""
-    return parse(Path(__file__).parent / "minimal_record.xml")
+from _pytest.fixtures import FixtureFunctionMarker
+from invenio_app.factory import create_api as _create_api
 
 
 @pytest.fixture(scope="module")
-def create_app(instance_path: str) -> Flask:
+def create_app(instance_path: FixtureFunctionMarker) -> Callable:
     """Application factory fixture."""
+    return _create_api
 
-    def factory(**config: dict) -> Flask:
-        app = Flask("testapp", instance_path=instance_path)
-        app.config.update(**config)
-        InvenioCampusonline(app)
-        return app
 
-    return factory
+@pytest.fixture
+def minimal_record() -> Element:
+    """Create minimal record."""
+    return parse(Path(__file__).parent / "minimal_record.xml")
