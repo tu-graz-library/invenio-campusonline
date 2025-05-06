62422f9127f2adfb8d126d7db5243419d7d4fc24
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-05-06 23:41:12 +0200

    jobs: add logger infos

--- invenio_campusonline/tasks.py
+++ invenio_campusonline/tasks.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2022-2024 Graz University of Technology.
+# Copyright (C) 2022-2025 Graz University of Technology.
 #
 # invenio-campusonline is free software; you can redistribute it
 # and/or modify it under the terms of the MIT License; see LICENSE
@@ -20,19 +20,22 @@ from .proxies import current_campusonline
 @shared_task(ignore_result=True)
 def import_theses_from_campusonline() -> None:
     """Import theses from campusonline."""
+    current_app.logger.info("start importing theses from campusonline")
+
     import_func = current_app.config["CAMPUSONLINE_IMPORT_FUNC"]
     theses_filter = current_app.config["CAMPUSONLINE_THESES_FILTER"]
 
     cms_service = current_campusonline.campusonline_rest_service
     ids = cms_service.fetch_all_ids(system_identity, theses_filter)
 
+    current_app.logger.info("%s records will be imported", len(ids))
+
     for cms_id in ids:
         try:
-            import_func(system_identity, cms_id, cms_service)
+            draft = import_func(system_identity, cms_id, cms_service)
+
+            msg = "campusonline draft.id: %s as been imported successfully"
+            current_app.logger.info(msg, draft.id)
         except RuntimeError as e:
             msg = "ERROR campusonline cms_id: %s couldn't be imported because of %s"
-            current_app.logger.error(
-                msg,
-                cms_id,
-                str(e),
-            )
+            current_app.logger.error(msg, cms_id, str(e))
